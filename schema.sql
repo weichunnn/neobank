0d6045b027ff789007758ad8d3bec92ebfd28e48
Wei Chun <tanweichun@outlook.com>
2022-08-11 22:36:28 +0800

    added bank schema file

---
+++ schema.sql
@@ -0,0 +1,51 @@
+drop table if exists "accounts" cascade;
+drop table if exists "entries" cascade;
+drop table if exists "transfers" cascade;
+
+create table "accounts" (
+  "id" bigserial primary key,
+  "owner" varchar not null,
+  "currency" varchar not null,
+  "created_at" timestamptz not null default (now())
+);
+
+create table "entries" (
+  "id" bigserial primary key,
+  "account_id" bigint not null,
+  "amount" bigint not null, 
+  "created_at" timestamptz not null default (now())
+);
+
+create table "transfers" (
+  "id" bigserial primary key,
+  "from_account_id" bigint not null,
+  "to_account_id" bigint not null,
+  "amount" bigint not null,
+  "created_at" timestamptz not null default (now())
+);
+
+alter table "entries" add foreign key ("account_id") references "accounts" ("id");
+
+alter table "transfers" add foreign key ("from_account_id") references "accounts" ("id");
+
+alter table "transfers" add foreign key ("to_account_id") references "accounts" ("id");
+
+create index on "accounts" ("owner");
+
+create index on "entries" ("account_id");
+
+create index on "transfers" ("from_account_id");
+
+create index on "transfers" ("to_account_id");
+
+create index on "transfers" ("from_account_id", "to_account_id");
+
+comment on
+  column "entries"."amount" is 'can be negative or positive';
+
+comment on
+  column "transfers"."amount" is 'must be positive';
+
+
+
+
