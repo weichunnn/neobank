6d8dc256d0a0678a9dc8d9bc6910149ca35cc7e7
Wei Chun <tanweichun@outlook.com>
2022-08-23 02:43:40 +0800

    remove select to favor update

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -95,34 +95,20 @@ func (store *Store) TransferTx(ctx context.Context, arg TranferTxParams) (Transf
 		}
 
 		// lock to prevent other transactions interfering with current operations
-		// fmt.Println(txName, "get account 1")
-		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
-
-		if err != nil {
-			return err
-		}
-
 		// fmt.Println(txName, "update account 1")
-		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
-			ID:      arg.FromAccountID,
-			Balance: account1.Balance - arg.Amount,
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
 		})
 
 		if err != nil {
 			return err
 		}
 
-		// fmt.Println(txName, "get account 2")
-		account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
-
-		if err != nil {
-			return err
-		}
-
 		// fmt.Println(txName, "update account 2")
-		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
-			ID:      arg.ToAccountID,
-			Balance: account2.Balance + arg.Amount,
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
 		})
 
 		if err != nil {
