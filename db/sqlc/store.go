ec514d10e859b9a2dc64f8e872928a4a0f2fd9b5
Wei Chun <tanweichun@outlook.com>
2022-08-25 02:24:40 +0800

    refactor fix for deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -77,7 +77,6 @@ func (store *Store) TransferTx(ctx context.Context, arg TranferTxParams) (Transf
 			AccountID: arg.FromAccountID,
 			Amount:    -arg.Amount,
 		})
-
 		if err != nil {
 			return err
 		}
@@ -87,7 +86,6 @@ func (store *Store) TransferTx(ctx context.Context, arg TranferTxParams) (Transf
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
 		})
-
 		if err != nil {
 			return err
 		}
@@ -95,47 +93,37 @@ func (store *Store) TransferTx(ctx context.Context, arg TranferTxParams) (Transf
 		// lock to prevent other transactions interfering with current operations
 		// fmt.Println(txName, "update account 1")
 		if arg.FromAccountID < arg.ToAccountID {
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.FromAccountID,
-				Amount: -arg.Amount,
-			})
-
-			if err != nil {
-				return err
-			}
-
-			// fmt.Println(txName, "update account 2")
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.ToAccountID,
-				Amount: arg.Amount,
-			})
-
-			if err != nil {
-				return err
-			}
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 		} else {
-			// fmt.Println(txName, "update account 2")
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.ToAccountID,
-				Amount: arg.Amount,
-			})
-
-			if err != nil {
-				return err
-			}
-
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.FromAccountID,
-				Amount: -arg.Amount,
-			})
-
-			if err != nil {
-				return err
-			}
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
+		if err != nil {
+			return err
 		}
 
-		return nil
+		return nil // base case
 	})
 
 	return result, err
+}
+
+// add money to different account as part of a transaction
+func addMoney(ctx context.Context, q *Queries, accountID1 int64, amount1 int64, accountID2 int64, amount2 int64) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	if err != nil {
+		return
+	}
+
+	return
 }
