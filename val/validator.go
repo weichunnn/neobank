773537b6869b4f499c46bd3ba97a3134e25e800f
Wei Chun <tanweichun@outlook.com>
2022-09-08 22:49:54 +0800

    added input validation scheme

---
+++ val/validator.go
@@ -0,0 +1,55 @@
+package val
+
+import (
+	"fmt"
+	"net/mail"
+	"regexp"
+)
+
+var (
+	isValidUsername = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
+	isValidFullname = regexp.MustCompile(`^[a-zA-Z\\s]+$`).MatchString
+)
+
+func ValidateString(value string, minLength int, maxLength int) error {
+	n := len(value)
+	if n < minLength || n > maxLength {
+		return fmt.Errorf("must contains from %d - %d number of characters", minLength, maxLength)
+	}
+	return nil
+}
+
+func ValidateUsername(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+	if !isValidUsername(value) {
+		return fmt.Errorf("must only contains lowercase letters, digits and underscores")
+	}
+	return nil
+}
+
+func ValidatePassword(value string) error {
+	return ValidateString(value, 6, 100)
+}
+
+func ValidateEmail(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+	if _, err := mail.ParseAddress(value); err != nil {
+		return fmt.Errorf("address is not a valid email")
+
+	}
+	return nil
+}
+
+func ValidateFullName(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+	if !isValidFullname(value) {
+		return fmt.Errorf("must only contains letters and spaces")
+	}
+	return nil
+}
